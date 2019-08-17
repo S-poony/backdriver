60aacf6c7fc55659b86eb28f652c20b144f034f8
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2019-08-17 19:09:09 +0200

    bullets added

--- index.js
+++ index.js
@@ -6,20 +6,62 @@ class Hero {
         this.height = 10;
         this.angle = 45;
         this.el = document.getElementById("hero");
+        this.bullets = [];
+        window.addEventListener("click", _ => {
+            this.fire()
+        })
     }
     draw() {
-        this.el.setAttribute("x", this.x);
-        this.el.setAttribute("y", this.y);
+        let dx = this.height / 2;
+        this.el.setAttribute("x", this.x - dx);
+        this.el.setAttribute("y", this.y - dx);
         this.el.setAttribute("width", this.width);
         this.el.setAttribute("height", this.height);
-        let dx = this.height / 2;
         let angleDeg = this.angle * 360 / (2 * Math.PI);
         this.el.setAttribute("transform",
             `rotate(${angleDeg} ${this.x + dx} ${this.y + dx})`);
-
+        for (const bullet of this.bullets) {
+            bullet.draw();
+        }
     }
+    update(dt) {
+        this.pointTo(mouse);
+        for (const bullet of this.bullets) {
+            bullet.update(dt);
+        }
+    }
+
     pointTo(o) {
         this.angle = angleBetween(this, o);
+    }
+    fire() {
+        this.bullets.push(new Bullet(this));
+    }
+}
+
+class Bullet {
+    constructor(origin) {
+        this.x = origin.x;
+        this.y = origin.y;
+        this.angle = origin.angle;
+        this.radius = 10;
+        this.speed = 0.1;
+        this.el = document.createElementNS(
+            "http://www.w3.org/2000/svg",
+            "circle"
+        );
+        svg.appendChild(this.el);
+    }
+    draw() {
+        this.el.setAttribute("cx", this.x);
+        this.el.setAttribute("cy", this.y);
+        this.el.setAttribute("r", this.radius);
+        this.el.setAttribute("height", this.radius);
+    }
+    update(dt) {
+        let l = this.speed * dt;
+        this.x += Math.cos(this.angle) * l;
+        this.y += Math.sin(this.angle) * l;
     }
 }
 
@@ -42,16 +84,19 @@ function angleBetween(o1, o2) {
 
 let lastTime = Date.now();
 var hero = null;
+let svg = null;
 var mouse = new Mouse();
 
 function update(currentTime) {
     const dt = currentTime - lastTime;
-    hero.pointTo(mouse);
+    lastTime = currentTime;
+    hero.update(dt);
     hero.draw();
     requestAnimationFrame(update);
 }
 
 window.onload = function () {
     hero = new Hero();
+    svg = document.getElementById("gameSvg");
     update(Date.now());
 }
